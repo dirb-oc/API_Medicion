705465e4f9df512349a416c023f47b07a1df7fc6
Orlando Contreras <dirbdeving@gmail.com>
2024-10-27 18:19:48 -0500

    CRUD basico usuarios

--- App.py
+++ App.py
@@ -2,4 +2,8 @@ from fastapi import FastAPI
 from Router.User_Router import user
 
 app = FastAPI()
+@app.get("/")
+def Home():
+    return {"System": "Operation"}
+
 app.include_router(user)
--- Router/User_Router.py
+++ Router/User_Router.py
@@ -3,15 +3,11 @@ from Database.Base import db
 from Models.Model_User import Usuario
 from Schemas.Schema_User import UsuarioResponse, UsuarioCreate
 
-user = APIRouter()
+user = APIRouter(tags=["Usuario"])
 
-@user.get("/")
-def HelloWorld():
-    return {"message": "Funcionando"}
-
-# Obtener un usuario por ID
-@user.get("/usuarios/",  response_model=list[UsuarioResponse])
-def List_Usuarios():
+# Obtener los usuarios
+@user.get("/users",  response_model=list[UsuarioResponse])
+def List_Users():
     usuarios = db.query(Usuario).all()
     db.close()
     if usuarios is None:
@@ -19,20 +15,53 @@ def List_Usuarios():
     return usuarios
 
 # Obtener un usuario por ID
-@user.get("/usuario/{usuario_id}", response_model=UsuarioResponse)
-def Search_Usuario(usuario_id: int):
-    usuario = db.query(Usuario).filter(Usuario.id == usuario_id).first()
+@user.get("/user/{id}", response_model=UsuarioResponse)
+def Search_User(id: int):
+    usuario = db.query(Usuario).filter(Usuario.id == id).first()
     db.close()
     if usuario is None:
         raise HTTPException(status_code=404, detail="Usuario no encontrado")
     return usuario
 
 # Crear un nuevo usuario
-@user.post("/usuario/", response_model=UsuarioResponse)
-def Create_Usuario(usuario: UsuarioCreate):
+@user.post("/user", response_model=UsuarioResponse)
+def Create_User(usuario: UsuarioCreate):
     new_usuario = Usuario(nombre=usuario.nombre, correo=usuario.correo)
     db.add(new_usuario)
     db.commit()
     db.refresh(new_usuario)
     db.close()
     return new_usuario
+
+# Actualizar usuario
+@user.put("/user/{id}", response_model=UsuarioResponse)
+def update_User(id: int, usuario: UsuarioCreate):
+    # Busca al usuario existente
+    db_usuario = db.query(Usuario).filter(Usuario.id == id).first()
+    
+    # Verifica si el usuario existe
+    if db_usuario is None:
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
+    
+    # Actualiza los valores del usuario
+    db_usuario.nombre = usuario.nombre
+    db_usuario.correo = usuario.correo
+    
+    # Confirma los cambios
+    db.commit()
+    db.refresh(db_usuario)
+    
+    return db_usuario
+
+# Eliminiar Usuario
+@user.delete("/user/{id}", response_model=UsuarioResponse)
+def delete_User(id: int):
+    usuario = db.query(Usuario).filter(Usuario.id == id).first()
+    
+    if usuario is None:
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
+    
+    db.delete(usuario)
+    db.commit()
+    
+    return usuario
