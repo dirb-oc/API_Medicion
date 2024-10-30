dae28169a0879133b7f723f2fbef15f8d9248986
Orlando Contreras <dirbdeving@gmail.com>
2024-10-30 01:32:45 -0500

    requisitos

--- Database/Base.py
+++ Database/Base.py
@@ -2,7 +2,9 @@ from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "postgresql://postgres:1005185673@localhost:5432/Medidor"
+# DATABASE_URL = "postgresql://postgres:1005185673@localhost:5432/Medidor"
+DATABASE_URL = "postgresql://orlando:2TNKEuccZQY02IV0KQxENy7sykDingap@dpg-csgsgibtq21c73dv2fm0-a.oregon-postgres.render.com/mediciondb"
+
 
 # Crear el motor de la base de datos
 engine = create_engine(DATABASE_URL)
